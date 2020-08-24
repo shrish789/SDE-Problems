44a4b17a0230df99d0f7a81266ba8083427c24d3
shrish789 <44712688+shrish789@users.noreply.github.com>
2020-08-24 22:34:13 +0530

    MSB in O(1)

---
+++ msb.cpp
@@ -0,0 +1,48 @@
+/*
+
+Find MSB in O(1)
+
+*/
+
+// CPP program to find MSB number for given n. 
+#include <iostream> 
+using namespace std; 
+
+int setBitNumber(int n) 
+{ 
+	// Below steps set bits after 
+	// MSB (including MSB) 
+
+	// Suppose n is 273 (binary 
+	// is 100010001). It does following 
+	// 100010001 | 010001000 = 110011001 
+	n |= n >> 1; 
+
+	// This makes sure 4 bits 
+	// (From MSB and including MSB) 
+	// are set. It does following 
+	// 110011001 | 001100110 = 111111111 
+	n |= n >> 2; 
+
+	n |= n >> 4; 
+	n |= n >> 8; 
+	n |= n >> 16; 
+
+	// Increment n by 1 so that 
+	// there is only one set bit 
+	// which is just before original 
+	// MSB. n now becomes 1000000000 
+	n = n + 1; 
+
+	// Return original MSB after shifting. 
+	// n now becomes 100000000 
+	return (n >> 1); 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int n = 273; 
+	cout << setBitNumber(n); 
+	return 0; 
+} 
