398994cf1707cf4a009f6baeeeb601d4c36760cc
shrish789 <44712688+shrish789@users.noreply.github.com>
2022-02-05 23:30:11 +0530

    Update setmatrixzeroes.cpp

--- setmatrixzeroes.cpp
+++ setmatrixzeroes.cpp
@@ -42,39 +42,29 @@ Follow up:
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool isCol=false;
-        int R=matrix.size();
-        int C=matrix[0].size();
-
-        for (int i = 0; i < R; i++) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstColumn = false;
+        
+        for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
-                isCol = true;
+                firstColumn = true;
             }
-
-            for (int j = 1; j < C; j++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
                 }
             }
         }
-
-        for (int i = 1; i < R; i++) {
-            for (int j = 1; j < C; j++) {
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
-        }
-
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < C; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if (isCol) {
-            for (int i = 0; i < R; i++) {
+            if (firstColumn) {
                 matrix[i][0] = 0;
             }
         }
