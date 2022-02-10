2f87b21bf1aea230166d3135d5b3aed241cc8cd3
shrish789 <44712688+shrish789@users.noreply.github.com>
2022-02-10 23:40:09 +0530

    Update mergesortedarray.cpp

--- mergesortedarray.cpp
+++ mergesortedarray.cpp
@@ -21,29 +21,22 @@ Constraints:
 -> nums2.length == n
 
 */
-
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1,j=n-1;
-        int k=m+n-1;
-        if(i==-1){
-            nums1=nums2;
-            return;
-        }
-        if(j==-1){
-            return;
-        }
-        while(j>=0 && i>=0){
-            if(nums1[i]>=nums2[j]){
-                nums1[k--]=nums1[i--];
-            }
-            else{
-                nums1[k--]=nums2[j--];
+        int k = m + n;
+        while (m >= 1 && n >= 1) {
+            if (nums1[m - 1] >= nums2[n - 1]) {
+                nums1[--k] = nums1[--m];
+            } else {
+                nums1[--k] = nums2[--n];
             }
         }
-        while(j>=0){
-            nums1[k--]=nums2[j--];
+        while (m >= 1) {
+            nums1[--k] = nums1[--m];
+        }
+        while (n >= 1) {
+            nums1[--k] = nums2[--n];
         }
     }
 };
