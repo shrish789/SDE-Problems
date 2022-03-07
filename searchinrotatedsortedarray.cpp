977aa49cc942663362edd8c19b89ab5b4a760a09
shrish789 <44712688+shrish789@users.noreply.github.com>
2022-03-08 01:51:15 +0530

    Update searchinrotatedsortedarray.cpp

--- searchinrotatedsortedarray.cpp
+++ searchinrotatedsortedarray.cpp
@@ -32,6 +32,7 @@ nums is guranteed to be rotated at some pivot.
 
 */
 
+// FIRST APPROACH: Find pivot and then element
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -90,3 +91,28 @@ public:
         return mid;
     }
 };
+
+
+// SECOND APPROACH: Easier
+int search(vector < int > & nums, int target) {
+  int low = 0, high = nums.size() - 1; //<---step 1
+
+  while (low <= high) { //<--- step 2
+    int mid = (low + high) >> 1; //<----step 3
+    if (nums[mid] == target)
+      return mid; // <---step 4
+
+    if (nums[low] <= nums[mid]) { //<---step 5
+      if (nums[low] <= target && nums[mid] >= target)
+        high = mid - 1; //<---step 6 
+      else
+        low = mid + 1; //<---step 7
+    } else { //<---step 7
+      if (nums[mid] <= target && target <= nums[high])
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+  }
+  return -1; //<---step 8
+}
