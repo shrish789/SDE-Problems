ca659200b3ca56b3aa5ec819142648f55323deb9
shrish789 <44712688+shrish789@users.noreply.github.com>
2022-03-12 23:54:59 +0530

    Update LargestRectangleInHistogram.cpp

--- LargestRectangleInHistogram.cpp
+++ LargestRectangleInHistogram.cpp
@@ -52,3 +52,39 @@ public:
         return maxArea;
     }
 };
+
+
+// THIRD APPROACH: In single traversal of array
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size(), maxArea = -1, i;
+        int leftArray[n], rightArray[n];
+        stack<int> st;
+        for (i = 0; i < n; i++) {
+            if (st.empty()) {
+                leftArray[i] = 0;
+                st.push(i);
+                continue;
+            }
+            if (heights[i] > heights[st.top()]) {
+                st.push(i);
+            } else {
+                while (!st.empty() && heights[st.top()] >= heights[i]) {
+                    int curr = st.top();
+                    st.pop();
+                    int left = st.empty() ? 0 : st.top() + 1;
+                    maxArea = max(maxArea, heights[curr] * (i - left));
+                }
+                st.push(i);
+            }
+        }
+        while (!st.empty()) {
+            int curr = st.top();
+            st.pop();
+            int left = st.empty() ? 0 : st.top() + 1;
+            maxArea = max(maxArea, heights[curr] * (i - left));
+        }
+        return maxArea;
+    }
+};
