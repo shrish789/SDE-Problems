39c60ca9b139a39c083eabd6d0d704bf76fe7d40
shrish789 <44712688+shrish789@users.noreply.github.com>
2022-03-13 02:33:20 +0530

    Create StockSpanner.cpp

---
+++ StockSpanner.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/online-stock-span/
+
+class StockSpanner {
+public:
+    stack<pair<int, int>> st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int cnt = 1;
+        while (!st.empty() && st.top().first <= price) {
+            cnt += st.top().second;
+            st.pop();
+        }
+        st.push({price, cnt});
+        return cnt;
+    }
+};
