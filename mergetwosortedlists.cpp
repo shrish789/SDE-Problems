af3b858cea6c6acb741ae7753288452f8f533d19
shrish789 <44712688+shrish789@users.noreply.github.com>
2023-04-19 03:51:32 +0530

    Update mergetwosortedlists.cpp

--- mergetwosortedlists.cpp
+++ mergetwosortedlists.cpp
@@ -74,3 +74,37 @@ public:
         return head;
     }
 };
+
+// Iterative but better
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL) return list2;
+        if (list2 == NULL) return list1;
+        ListNode *head = list1, *temp;
+        if (list1 -> val < list2 -> val) {
+            list1 = list1 -> next;
+        } else {
+            head = list2;
+            list2 = list2 -> next;
+        }
+        temp = head;
+        while (list1 && list2) {
+            if (list1 -> val < list2 -> val) {
+                temp -> next = list1;
+                list1 = list1 -> next;
+            } else {
+                temp -> next = list2;
+                list2 = list2 -> next;
+            }
+            temp = temp -> next;
+        }
+
+        if (!list1) {
+            temp -> next = list2;
+        } else {
+            temp -> next = list1;
+        }
+        return head;
+    }
+};
