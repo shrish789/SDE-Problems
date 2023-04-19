4834e59323c0a0bca66ea34fa9b3f59605893cbf
shrish789 <44712688+shrish789@users.noreply.github.com>
2023-04-19 17:23:33 +0530

    Update and rename intersectionoftwolinkedlists.cpp to Intersection of Two Linked Lists.cpp

--- Intersection of Two Linked Lists.cpp
+++ Intersection of Two Linked Lists.cpp
@@ -61,6 +61,24 @@ public:
     }
 };
 
+
+// Using above principle but simple code
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* d1 = headA;
+        ListNode* d2 = headB;
+        
+        while(d1 != d2) {
+            d1 = d1 == NULL? headB : d1->next;
+            d2 = d2 == NULL? headA : d2->next;
+        }
+        
+        return d1;
+    }
+};
+
+
 // Using extra space
 class Solution {
 public:
