b5fabd29c4c38ecc2e877ccdf2ec127572277b4c
shrish789 <44712688+shrish789@users.noreply.github.com>
2023-04-21 19:41:59 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -31,20 +31,21 @@ class Solution {
 
 // SECOND APPROACH: Without using vis array
 class Solution {
+private:
+    void permuteUtil(vector <int> nums, int pos, vector <vector <int>> &res) {
+        if (pos == nums.size()) {
+            res.push_back(nums);
+        }
+        for (int i = pos; i < nums.size(); i++) {
+            swap(nums[i], nums[pos]);
+            permuteUtil(nums, pos + 1, res);
+            swap(nums[i], nums[pos]);
+        }
+    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> v;
-        backtrack(v, nums, 0);
-        return v;
-    }
-    void backtrack(vector<vector<int>> &v, vector<int> v1, int pos) {
-        if (pos == v1.size()) {
-            v.push_back(v1);
-        }
-        for (int i = pos; i < v1.size(); i++) {
-            swap(v1[pos], v1[i]);
-            backtrack(v, v1, pos + 1);
-            swap(v1[pos], v1[i]);
-        }
+        vector <vector <int>> res;
+        permuteUtil(nums, 0, res);
+        return res;
     }
 };
