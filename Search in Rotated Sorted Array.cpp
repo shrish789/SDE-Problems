9a249b2a17a43aa1e88560822aaa93629cc2b4d3
shrish789 <44712688+shrish789@users.noreply.github.com>
2023-04-26 03:20:12 +0530

    Update and rename searchinrotatedsortedarray.cpp to Search in Rotated Sorted Array.cpp

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1, mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) { // left half is sorted
+                if (target >= nums[low] && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else { // right half is sorted
+                if (target >= nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
