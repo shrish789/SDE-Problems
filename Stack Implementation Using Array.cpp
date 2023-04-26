1b6c8b0004dd84cc591fff6ccc9939e8d3b87ab7
shrish789 <44712688+shrish789@users.noreply.github.com>
2023-04-27 02:59:02 +0530

    Create Stack Implementation Using Array.cpp

---
+++ Stack Implementation Using Array.cpp
@@ -0,0 +1,49 @@
+// https://www.codingninjas.com/codestudio/problems/stack-implementation-using-array_3210209
+
+#include <bits/stdc++.h> 
+class Stack {
+private:
+    vector <int> v;
+    int cap;
+public:
+    Stack(int capacity) {
+        cap = capacity;
+    }
+
+    void push(int num) {
+        if (v.size() < cap) {
+            v.push_back(num);
+        }
+    }
+
+    int pop() {
+        if (v.size() != 0) {
+            int lastEle = v.back();
+            v.pop_back();
+            return lastEle;
+        }
+        return -1;
+    }
+    
+    int top() {
+        if (v.size() != 0) {
+            return v.back();
+        }
+        return -1;
+    }
+    
+    int isEmpty() {
+        if (v.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    int isFull() {
+        if (v.size() == cap) {
+            return true;
+        }
+        return false;
+    }
+    
+};
