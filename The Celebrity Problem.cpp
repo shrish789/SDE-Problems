dedd41ab6aaa0ebe218baebcadd0deee36eab902
shrish789 <44712688+shrish789@users.noreply.github.com>
2023-05-01 02:49:29 +0530

    Update and rename TheCelebrityProblem.cpp to The Celebrity Problem.cpp

--- The Celebrity Problem.cpp
+++ The Celebrity Problem.cpp
@@ -1,17 +1,12 @@
 // https://www.codingninjas.com/codestudio/problems/the-celebrity-problem_982769
 
 int findCelebrity(int n) {
-    int ans = -1;
-    for (int i = 0; i < n; i++) {
-        if (ans == -1) {
-            ans = i;
-        } else if ((knows(ans, i) && knows(i, ans)) || (!knows(ans, i) && !knows(i, ans))) {
-            ans = -1;
-        } else if(knows(ans, i)) {
+    int ans = 0;
+    for (int i = 1; i < n; i++) {
+        if (knows(ans, i)) {
             ans = i;
         }
     }
-    if (ans == -1) return ans;
     for (int i = 0; i < n; i++) {
         if (ans != i && (knows(ans, i) || !knows(i, ans)))
             return -1;
