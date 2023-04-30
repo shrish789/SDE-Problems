83a78b946f6bfaa333cec1fcbecdac99ea5d6418
shrish789 <44712688+shrish789@users.noreply.github.com>
2023-05-01 02:24:31 +0530

    Update Rotting Oranges.cpp

--- Rotting Oranges.cpp
+++ Rotting Oranges.cpp
@@ -2,20 +2,23 @@
 
 class Solution {
 private:
-    void bfs(vector<vector<int>>& grid) {
+    int bfs(vector<vector<int>>& grid) {
         queue <pair <int, int>> q;
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size(), cnt = 0, ans = 0;
         int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
                     q.push(make_pair(i, j));
+                } else if (grid[i][j] == 1) {
+                    cnt++;
                 }
             }
         }
 
         while (!q.empty()) {
             pair <int, int> p = q.front();
+            ans = max(ans, grid[p.first][p.second]);
             q.pop();
             for (int k = 0; k < 4; k++) {
                 int i = p.first + dir[k][0];
@@ -23,22 +26,15 @@ private:
                 if (i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == 1) {
                     q.push(make_pair(i, j));
                     grid[i][j] += grid[p.first][p.second];
+                    cnt--;
                 }
             }
         }
+        return cnt > 0 ? -1 : ans;
     }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        bfs(grid);
-        int ans = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                ans = max(ans, grid[i][j]);
-                if (grid[i][j] == 1) {
-                    return -1;
-                }
-            }
-        }
-        return ans > 0 ? ans - 2 : 0;
+        int ans = bfs(grid);
+        return ans <= 0 ? ans : ans - 2;
     }
 };
